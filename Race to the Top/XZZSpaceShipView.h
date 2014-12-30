5c64f0a72e567abc5960a5c4ec53532e292723b1
zac1st1k <zacfirst@gmail.com>
2014-12-31 01:52:14 +1100

    + Custom view

---
+++ Race to the Top/XZZSpaceShipView.h
@@ -0,0 +1,13 @@
+//
+//  XZZSpaceShipView.h
+//  Race to the Top
+//
+//  Created by Zac on 31/12/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZSpaceShipView : UIView
+
+@end
---
+++ Race to the Top/XZZSpaceShipView.m
@@ -0,0 +1,30 @@
+//
+//  XZZSpaceShipView.m
+//  Race to the Top
+//
+//  Created by Zac on 31/12/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import "XZZSpaceShipView.h"
+
+@implementation XZZSpaceShipView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+    
+}
+
+@end
