d7eb64d8fd57d91663380f971c5c5cae0cc3436d
zac1st1k <zacfirst@gmail.com>
2014-12-31 01:46:27 +1100

    Initial Commit

---
+++ Race to the TopTests/Race_to_the_TopTests.m
@@ -0,0 +1,34 @@
+//
+//  Race_to_the_TopTests.m
+//  Race to the TopTests
+//
+//  Created by Zac on 31/12/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Race_to_the_TopTests : XCTestCase
+
+@end
+
+@implementation Race_to_the_TopTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
