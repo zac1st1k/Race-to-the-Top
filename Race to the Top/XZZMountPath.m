33723487b8fccac37555758ac3ddff60ecfa7513
zac1st1k <zacfirst@gmail.com>
2015-01-01 20:21:49 +1100

    + Complete bezier labyrinth

--- Race to the Top/XZZMountPath.m
+++ Race to the Top/XZZMountPath.m
@@ -10,18 +10,121 @@
 
 @implementation XZZMountPath
 
-+ (NSArray *)mountainPathsForRect:(CGRect)rect;
+//+ (NSArray *)mountainPathsForRect:(CGRect)rect;
+//{
+//    NSMutableArray *variousPaths = [@[] mutableCopy];
+//    CGPoint firstPoint = CGPointMake(rect.size.width * (1/6.0), CGRectGetMaxY(rect));
+//    CGPoint secondPoint = CGPointMake(rect.size.width * (1/3.0), rect.size.height * (5/6.0));
+//    UIBezierPath *labyrinthPath = [UIBezierPath bezierPath];
+//    labyrinthPath.lineWidth = 4.0;
+//    [labyrinthPath moveToPoint:firstPoint];
+//    [labyrinthPath addLineToPoint:secondPoint];
+//    [variousPaths addObject:labyrinthPath];
+//    
+//    return variousPaths;
+//    
+//}
+
++ (NSArray *)mountainPathsForRect:(CGRect)rect
+
 {
-    NSMutableArray *variousPaths = [@[] mutableCopy];
-    CGPoint firstPoint = CGPointMake(rect.size.width * (1/6.0), CGRectGetMaxY(rect));
-    CGPoint secondPoint = CGPointMake(rect.size.width * (1/3.0), rect.size.height * (5/6.0));
-    UIBezierPath *labyrinthPath = [UIBezierPath bezierPath];
-    labyrinthPath.lineWidth = 4.0;
-    [labyrinthPath moveToPoint:firstPoint];
-    [labyrinthPath addLineToPoint:secondPoint];
-    [variousPaths addObject:labyrinthPath];
     
-    return variousPaths;
+    NSMutableArray *variousPaths = [@[] mutableCopy];
+    
+    // First Path
+    
+    CGPoint firstPoint = CGPointMake(rect.size.width * (1/6.0), CGRectGetMaxY(rect));
+    
+    CGPoint secondPoint = CGPointMake(rect.size.width * (1/3.0), rect.size.height * (5/6.0));
+    
+    CGPoint thirdPoint = CGPointMake(CGRectGetMaxX(rect), rect.size.height * (5/6.0));
+    
+    CGPoint fourthPoint = CGPointMake(CGRectGetMaxX(rect), rect.size.height * (2/3.0));
+    
+    CGPoint fifthPoint = CGPointMake(rect.size.width * (1/6.0), rect.size.height * (6/12.0));
+    
+    CGPoint sixthPoint = CGPointMake(rect.size.width * (1/6.0), rect.size.height * (6/12.0));
+    
+    CGPoint seventhPoint = CGPointMake(rect.size.width * (1/3.0), rect.size.height * (2/6.0));
+    
+    CGPoint eigthPoint = CGPointMake(rect.size.width * (2/3.0), rect.size.height * (6/12.0));
+    
+    CGPoint ninthPoint = CGPointMake(rect.size.width * (11/20.0), rect.size.height * (5/24.0));
+    
+    CGPoint controlPoint = CGPointMake(rect.size.width * (6 / 8.0), rect.size.height * (1/ 3.0));
+    
+    // Second Path
+    
+    CGPoint firstPointForSecondPath = CGPointMake(rect.size.width * (1/6.0) - PATH_WIDTH, CGRectGetMaxY(rect));
+    
+    CGPoint secondPointForSecondPath = CGPointMake(rect.size.width * (1/3.0), rect.size.height * (5/6.0) - PATH_WIDTH);
+    
+    CGPoint thirdPointForSecondPath = CGPointMake(CGRectGetMaxX(rect) - PATH_WIDTH, rect.size.height * (5/6.0) - PATH_WIDTH);
+    
+    CGPoint fourthPointForSecondPath = CGPointMake(CGRectGetMaxX(rect)- PATH_WIDTH, rect.size.height * (2/3.0) + PATH_WIDTH);
+    
+    CGPoint fifthPointForSecondPath = CGPointMake(rect.size.width * (1/6.0) - PATH_WIDTH, rect.size.height * (6/12.0) + PATH_WIDTH / 2);
+    
+    CGPoint sixthPointForSecondPath = CGPointMake(rect.size.width * (1/6.0) - PATH_WIDTH, rect.size.height * (6/12.0));
+    
+    CGPoint seventhPointForSecondPath = CGPointMake(rect.size.width * (1/3.0) - PATH_WIDTH / 4, rect.size.height * (2/6.0) - PATH_WIDTH);
+    
+    CGPoint eigthPointForSecondPath = CGPointMake(rect.size.width * (2/3.0) - PATH_WIDTH, rect.size.height * (6/12.0) - PATH_WIDTH);
+    
+    CGPoint ninthPointForSecondPath = CGPointMake(rect.size.width * (11/20.0) - PATH_WIDTH, rect.size.height * (5/24.0));
+    
+    CGPoint controlPointForSecondpath = CGPointMake(rect.size.width * (6 / 8.0) - PATH_WIDTH, rect.size.height * (1/ 3.0));
+    
+    UIBezierPath *rightMountainPath = [UIBezierPath bezierPath]; // alternative to alloc, init, nested method calls
+    
+    UIBezierPath *leftMountainPath = [UIBezierPath bezierPath];
+    
+    rightMountainPath.lineWidth = 4.0;
+    
+    leftMountainPath.lineWidth = 4.0;
+    
+    [rightMountainPath moveToPoint:firstPoint];
+    
+    [rightMountainPath addLineToPoint:secondPoint];
+    
+    [rightMountainPath addLineToPoint:thirdPoint];
+    
+    [rightMountainPath addLineToPoint:fourthPoint];
+    
+    [rightMountainPath addLineToPoint:fifthPoint];
+    
+    [rightMountainPath addLineToPoint:sixthPoint];
+    
+    [rightMountainPath addLineToPoint:seventhPoint];
+    
+    [rightMountainPath addLineToPoint:eigthPoint];
+    
+    [rightMountainPath addQuadCurveToPoint:ninthPoint controlPoint:controlPoint];
+    
+    [leftMountainPath moveToPoint:firstPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:secondPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:thirdPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:fourthPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:fifthPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:sixthPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:seventhPointForSecondPath];
+    
+    [leftMountainPath addLineToPoint:eigthPointForSecondPath];
+    
+    [leftMountainPath addQuadCurveToPoint:ninthPointForSecondPath controlPoint:controlPointForSecondpath];
+    
+    [variousPaths addObject:rightMountainPath];
+    
+    [variousPaths addObject:leftMountainPath];
+    
+    return [variousPaths copy];
+    
 }
 
 @end
